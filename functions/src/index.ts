8e363242d72abbdedc76a1c43b01fcf0c13df7b5
Kalan Jarvis-Loewen <karj5903@gmail.com>
2025-05-25 18:10:42 -0700

    fixed twillio functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 // Import necessary Firebase functions
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { onDocumentWritten } from 'firebase-functions/firestore';
+
+const logger = functions.logger
 
 // Initialize Firebase Admin SDK
 // This allows the Cloud Function to interact with Firestore and other Firebase services
@@ -139,49 +142,49 @@ exports.submitBooking = functions.https.onCall(async (request: functions.https.C
   }
 });
 
-// exports.twillioFirestoreReroute = onDocumentWritten("messages/{docId}", async (event) => {
-//   const change: any = event.data;
+exports.twillioFirestoreReroute = onDocumentWritten("contact/{docId}", async (event) => {
+  const change: any = event.data;
 
-//   if (!change) {
-//     logger.error("No change data found.");
-//     return;
-//   }
+  if (!change) {
+    logger.error("No change data found.");
+    return;
+  }
 
-//   const afterDoc = change.after;
+  const afterDoc = change.after;
 
-//   if (afterDoc.exists) {
-//     const messageData = afterDoc.data();
+  if (afterDoc.exists) {
+    const messageData = afterDoc.data();
 
-//     if (messageData) {
-//       try {
-//         const smsRef = admin.firestore().collection("sms").doc();
+    if (messageData) {
+      try {
+        const smsRef = admin.firestore().collection("sms").doc();
 
-//         await smsRef.set({
-//           ...messageData,
-//           createdAt: admin.firestore.FieldValue.serverTimestamp(),
-//           to: "+12366680975",
-//           body: 
-//           `New message from "${messageData.name}", "${messageData.email}" Message: "${messageData.message}"`
-//         });
-//         await smsRef.set({
-//           ...messageData,
-//           createdAt: admin.firestore.FieldValue.serverTimestamp(),
-//           to: "",
-//           body: 
-//           `New message from "${messageData.name}", "${messageData.email}"Message: "${messageData.message}"`
-//         });
+        await smsRef.set({
+          ...messageData,
+          createdAt: admin.firestore.FieldValue.serverTimestamp(),
+          to: "+12366680975",
+          body: 
+          `New message from "${messageData.name}", "${messageData.email}" Message: "${messageData.message}"`
+        });
+        await smsRef.set({
+          ...messageData,
+          createdAt: admin.firestore.FieldValue.serverTimestamp(),
+          to: "",
+          body: 
+          `New message from "${messageData.name}", "${messageData.email}"Message: "${messageData.message}"`
+        });
 
-//         logger.info(
-//           `Document added to 'sms' collection with ID: ${smsRef.id}`
-//         );
-//       } catch (error) {
-//         logger.error("Error adding document to 'sms' collection:", error);
-//       }
-//     }
-//   } else {
-//     logger.info("Document was deleted or does not exist after the change.");
-//   }
-// });
+        logger.info(
+          `Document added to 'sms' collection with ID: ${smsRef.id}`
+        );
+      } catch (error) {
+        logger.error("Error adding document to 'sms' collection:", error);
+      }
+    }
+  } else {
+    logger.info("Document was deleted or does not exist after the change.");
+  }
+});
 
 
 
