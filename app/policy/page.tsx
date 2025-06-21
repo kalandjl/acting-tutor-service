b7349f4b78ec696fb32258b267fedc750bd251bb
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-06-21 15:12:30 -0700

    policy all on the same page

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -6,43 +6,36 @@ const Home = () => {
     return (
         <>  
             <Layout navColor="stone-800">
-                <section className="grid xl:px-96 lg:px-64 md:px-48 place-items-center min-h-screen">
+                <section className="grid xl:px-96 lg:px-64 md:px-48 place-items-center min-h-screen mt-10">
                     <div className="h-screen mt-10">
-                        <div id="header-wrap" className="py-10 w-max grid place-items-center transition ease-in-out hover:scale-105">
-                            <h1 className="md:text-4xl text-2xl font-bold">
+                        <div id="header-wrap" className="pt-10 pb-5 grid place-items-center transition ease-in-out hover:scale-105">
+                            <h1 className="md:text-4xl text-2xl font-bold text-center ">
                                 Booking Policy
                             </h1>
                         </div>
+           
                         <div id="booking-policy">
-                            <div id="subheader-wrap">
-                                <h1 className="text-2xl font-semibold">
-                                    Booking
-                                </h1>
-                            </div>
-                            <div id="text-wrap">
+                            <div id="text-wrap" className="text-center">
                                 <div>
-                                    <h1 className="font-bold text-md mt-3">Can a parent attend a youth coaching session?</h1>
+                                    <h1 className="font-bold text-lg">Can a parent attend a youth coaching session?</h1>
                                     <p>
                                         For underage actors, a student can be accompanied with their parent for the entire duration of the coaching session.
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div id="cancelation-policy" className="w-full mt-10">
-                            <div id="subheader-wrap" className="w-max">
-                                <h1 className="text-2xl font-semibold">
-                                    Cancellation
-                                </h1>
+                            <div className="h-screen mt-20">
+                                <div id="header-wrap" className="py-5 grid place-items-center">
+                                    <h1 className="md:text-4xl text-2xl font-bold text-center w-max">
+                                        Cancellation Policy
+                                    </h1>
+                                </div> 
+                                <div id="policy-wrap" className="text-center">
+                                    <p className="font">
+                                        Out of respect for your coach, please note that cancellations made within twelve (12) hours of your scheduled coaching session will be subject to a $25 cancellation fee. Cancellations made within one (1) hour will be subject to the full session fee.
+                                    </p>
+                                </div>
                             </div>
-                            <div id="text-wrap">
-                                <Link href="/policy/cancelation">
-                                    <span className='relative group text-blue-700'>
-                                        Cancelation Policy
-                                        <span className={`absolute left-0 -bottom-1 w-0 h-0.5 bg-blue-700 transition-all duration-300 group-hover:w-full`}></span>
-                                    </span>                            
-                                </Link>
-                            </div>
-                        </div>
                     </div>
                 </section>
             </Layout>
