ccb29b3664b319c41cc1eea7274689298f334619
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-06-29 18:22:31 -0700

    favicons

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 import pages from "@/res/links";
+import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
+import Logo from "@/public/icon.png"
 
  
 interface Props {
@@ -15,7 +17,9 @@ const Nav: FC<Props> = (props) => {
                 <div id="nav-inner" className="flex justify-between min-h-24 h-24">
                     <div id="logo" className="h-full p-4 w-24">
                         <Link href="/" className="">
-                            <div id="mock-logo" className=" rounded-md h-full w-full transition ease-in-out hover:scale-105 hover:cursor-pointer"></div>
+                            <div className="relative h-full">
+                                <Image src={Logo} layout="fill" alt="" className="absolute inset-0 brightness-0 invert" />
+                            </div>
                         </Link>
                     </div>
                     <div id="links" className="grid gap-16 grid-flow-col h-full">
