56de0c9f1906fc4f33f8ed401e7acda3aed3203d
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-06-30 23:08:52 -0700

    public

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,14 +9,23 @@ import { getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+
+  apiKey: "AIzaSyCcJXHOgbnM-iGs_HeTamaoRMMOv5cJZzg",
+
+  authDomain: "acting-tutoring-service.firebaseapp.com",
+
+  projectId: "acting-tutoring-service",
+
+  storageBucket: "acting-tutoring-service.firebasestorage.app",
+
+  messagingSenderId: "1041796714673",
+
+  appId: "1:1041796714673:web:07cbd4f11de0d4eb4f5290",
+
+  measurementId: "G-3CS9QLSNS4"
+
 };
+
 
 
 // Initialize Firebase
